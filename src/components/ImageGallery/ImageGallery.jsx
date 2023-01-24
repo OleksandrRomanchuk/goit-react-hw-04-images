adfd299d090683a30104499115eb72b37aaa4c69
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-24 13:21:43 +0200

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -6,21 +6,19 @@ import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 //========== styles ==========
 import css from './ImageGallery.module.css';
 
-export function ImageGallery({ items, modalOpen, getImageData }) {
-    return <ul className={css.ImageGallery}>
+export function ImageGallery({ items, getImageData }) {
+    return <ul className={css.ImageGallery} id="gallery">
         {items
             .map(item =>
                 <ImageGalleryItem
                     key={item.id}
                     photoData={item}
-                    openModal={modalOpen}
                     getImageData={getImageData}
                 />)}
     </ul>;
 };
 
 ImageGallery.propTypes = {
-    modalOpen: PropTypes.func.isRequired,
     getImageData: PropTypes.func.isRequired,
     items: PropTypes.arrayOf(PropTypes.shape({
         largeImageURL: PropTypes.string.isRequired,
