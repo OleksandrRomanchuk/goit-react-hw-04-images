103082f62d5433d9f930c4cd046e5692d6d0cbc5
Oleksandr Romanchuk <novator0511@gmail.com>
2023-01-24 13:21:34 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,44 +2,42 @@ import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 
 //========== components ==========
-import { Component } from 'react';
+import { useEffect } from 'react';
 
 //========== styles ==========
 import css from './Modal.module.css';
 
 const modalPortal = document.getElementById('modal-root');
 
-class Modal extends Component {
-    static propTypes = {
-            modalToggle: PropTypes.func.isRequired,
-            children: PropTypes.node.isRequired,
-    };
-
-    componentDidMount() {
-        window.addEventListener('keydown', this.onKeyDown)
-    };
-
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.onKeyDown)
-    };
-
-    onKeyDown = (event) => {
+export function Modal({ modalToggle, children }) {
+    useEffect(() => {
+        const onKeyDown = (event) => {
         if (event.code === 'Escape') {
-            this.props.modalToggle();
+            modalToggle();
+            };
         };
-    };
+        
+        window.addEventListener('keydown', onKeyDown);
 
-    onModalOpen = (event) => {
+        return () => {
+            window.removeEventListener('keydown', onKeyDown);
+        };
+    }, [modalToggle]);
+    
+    const onModalOpen = (event) => {
         if (event.target.nodeName !== 'IMG') {
-            this.props.modalToggle();
+            modalToggle();
         };
     };
 
-    render() {
-        return createPortal(<div className={css.Overlay} onClick={this.onModalOpen}>
-            <div className={css.Modal}>{this.props.children}</div>
-        </div>, modalPortal);
-    };
+    return createPortal(<div className={css.Overlay} onClick={onModalOpen}>
+        <div className={css.Modal}>
+            {children}
+        </div>
+    </div>, modalPortal);
 };
 
-export { Modal };
+Modal.propTypes = {
+    modalToggle: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired,
+};
